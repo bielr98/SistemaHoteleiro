542257e9e1e5683911917441675925485caa2d3c
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-05-07 02:20:50 -0300

    Criadas Variaveis iniciais do Quarto

--- src/Quarto.java
+++ src/Quarto.java
@@ -1,2 +1,15 @@
 public class Quarto {
+    private int numeroDoQuarto;
+    private final int capacidadeMaxima;
+    private int hospedesAtualmente;
+    private boolean ocupado;
+    private boolean chaveNaRecepcao;
+
+    public Quarto(int numeroDoQuarto) {
+        this.numeroDoQuarto = numeroDoQuarto; // Número do quarto é definido na criação
+        this.capacidadeMaxima = 4; // Capacidade máxima é sempre 4 e não muda
+        this.hospedesAtualmente = 0; // Inicia com 0 hóspedes
+        this.ocupado = false; // Inicia como não ocupado
+        this.chaveNaRecepcao = true; // Chave começa na recepção
+    }
 }
