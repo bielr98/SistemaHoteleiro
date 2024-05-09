50b87dea8ca7d4c77dd4e6a65fc26c79ff67beb8
Gabriel Ribeiro Noronha <gabrielrib98@gmail.com>
2024-05-09 03:57:45 -0300

    Comentarios para Chave.java

--- src/Chave.java
+++ src/Chave.java
@@ -1,15 +1,21 @@
 public class Chave {
+    // Referência ao quarto associado à chave
     private Quarto quarto;
+    // Referência ao hóspede que possui a chave
     private Hospede hospede;
 
+    // Construtor que inicializa a chave com um hóspede e um quarto associados
     public Chave(Hospede hospede, Quarto quarto) {
-        this.quarto = quarto;
+        this.hospede = hospede;  // Armazena o hóspede associado à chave
+        this.quarto = quarto;    // Armazena o quarto associado à chave
     }
 
+    // Método para obter o quarto associado à chave
     public Quarto getQuarto() {
         return quarto;
     }
 
+    // Método para obter o hóspede associado à chave
     public Hospede getHospede() {
         return hospede;
     }
